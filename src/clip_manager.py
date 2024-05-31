f9c529be0dbeec4a244b50e27e59acbe0aec3a2f
Jerry Qu <32346032+Jerry2001Qu@users.noreply.github.com>
2024-05-31 14:07:07 +0000

    debug

--- src/clip_manager.py
+++ src/clip_manager.py
@@ -109,11 +109,11 @@ class ClipManager:
 
         # Combine clips that match the same sot and are either next to each other or have one clip in between
         i = 0
-        self.error_handler.warning(len(self.clips)-1)
         while i < len(self.clips) - 1:
             current_clip = self.clips[i]
             next_clip = self.clips[i + 1]
-            if current_clip.shot_id == next_clip.shot_id:
+            self.error_handler.warning(f"{current_clip.id}, {next_clip.id}: {current_clip.shot_id}, {next_clip.shot_id}")
+            if current_clip is not None and next_clip is not None and current_clip.shot_id == next_clip.shot_id:
                 current_clip.file_path = self.combine_clips(current_clip, next_clip)
                 self.clips.pop(i + 1)  # Remove next_clip after combining
                 if self.error_handler:
