fffc3db6a71d8f073cdd1b0b98ac43cbd5727505
Jerry Qu <32346032+Jerry2001Qu@users.noreply.github.com>
2024-06-07 19:32:52 +0000

    fix hashing file not found

--- src/hashing.py
+++ src/hashing.py
@@ -1,5 +1,9 @@
 import hashlib
+from pathlib import Path
 
-def sha256sum(file):
-    with open(file, 'rb', buffering=0) as f:
-        return hashlib.file_digest(f, 'sha256').hexdigest()
+def sha256sum(file: Path):
+    if file.exists():
+        with open(file, 'rb', buffering=0) as f:
+            return hashlib.file_digest(f, 'sha256').hexdigest()
+    else:
+        return "A"
