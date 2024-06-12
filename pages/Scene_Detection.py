aae1013d8b10da49776bdf834a652932b8d49edd
Jerry Qu <32346032+Jerry2001Qu@users.noreply.github.com>
2024-06-12 23:05:39 +0000

    add scene detection page

---
+++ pages/Scene_Detection.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from streamlit.logger import get_logger
+
+LOGGER = get_logger(__name__)
+
+from scenedetect import detect, AdaptiveDetector, split_video_ffmpeg, ContentDetector
+from pathlib import Path
+
+def run():
+    st.set_page_config(
+        page_title="Channel 1",
+        page_icon="👋",
+        layout="wide"
+    )
+
+    st.write("# Channel 1 Demo")
+
+    def save_uploaded(file_data):
+        with open("/tmp/video.mp4" ,"wb") as f:
+            f.write(file_data.getbuffer())
+    
+    detector_name = st.selectbox("Detection method", ["Adaptive", "Content"])
+
+    if detector_name == "Adaptive":
+        threshold = st.slider("Threshold", 0, 10, 4)
+        detector = AdaptiveDetector(adaptive_threshold=threshold, min_scene_len=1)
+    elif detector_name == "Content":
+        threshold = st.slider("Threshold", 0, 100, 27)
+        detector = ContentDetector(threshold=threshold, min_scene_len=1)
+
+    video = st.file_uploader("Video", type=["mp4"], accept_multiple_files=False)
+    if video:
+        save_uploaded(video)
+        with st.expander("Uploaded video"):
+            st.video("/tmp/video.mp4")
+        
+        clips_folder = Path("/tmp/clips")
+        video_file_path = Path("/tmp/video.mp4")
+        clips_folder.mkdir(parents=True, exist_ok=True)
+        scene_list = detect(str(video_file_path), detector)
+        status = split_video_ffmpeg(str(video_file_path), scene_list, show_progress=False,
+                        output_file_template=str(clips_folder / "$SCENE_NUMBER.mp4"))
+        
+        for video_path in clips_folder.glob("*.mp4"):
+            st.video(str(video_path))
+
+if __name__ == "__main__":
+    run()
