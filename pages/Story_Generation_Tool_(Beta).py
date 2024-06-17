40912c897c81447a457c94c1979c4f9e55de71f4
Jerry Qu <32346032+Jerry2001Qu@users.noreply.github.com>
2024-06-17 17:54:42 +0000

    remove profiler

--- pages/Story_Generation_Tool_(Beta).py
+++ pages/Story_Generation_Tool_(Beta).py
@@ -26,17 +26,6 @@ from src.authentication import check_password
 
 from src.prompts import cache
 
-from streamlit_profiler import Profiler
-
-class DummyContextManager:
-    def __enter__(self):
-        # Do nothing when entering the context
-        pass
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        # Do nothing when exiting the context
-        pass
-
 def run():
     if not check_password():
         st.stop()
@@ -77,7 +66,6 @@ def run():
     live_anchor = st.toggle("Motion Anchor", value=True)
     high_res_anchor = st.toggle("High Res Anchor", value=True)
     music = st.toggle("Add Music", value=True)
-    profile = st.toggle("Enable Profiler", value=False)
     test_mode = not high_res_anchor
 
     languages = ["English", "Spanish", "French", "German", "Polish", "Italian", "Portuguese", "Russian", "Arabic", "Dutch", "Swedish", "Norwegian", "Turkish", "Japanese", "Korean", "Filipino", "Tamil", "Indonesian", "Greek", "Chinese"]
@@ -110,153 +98,152 @@ def run():
     clean_reuters_id = "".join(filter(lambda x: x.isalnum() or x.isspace(), reuters_id))
     if st.button("Download Story Assets"):
         st.session_state["download_run"] = True
+
+    if st.session_state["download_run"]:
+        story_folder = Path("/tmp") / clean_reuters_id
+        dataloader = ReutersAPIDataLoader(reuters_id, story_folder)
+        storyline = dataloader.load_storyline()
+        shotlist = dataloader.load_shotlist()
+        story_title = dataloader.get_story_title()
+        video_file_path = dataloader.get_video_file_path()
+
+        st.title(story_title)
+        st.video(str(video_file_path))
+        st.write(storyline.replace("$", "\$"))
+        st.write(shotlist.replace("$", "\$"))
     
-    with Profiler() if profile else DummyContextManager():
-        if st.session_state["download_run"]:
-            story_folder = Path("/tmp") / clean_reuters_id
-            dataloader = ReutersAPIDataLoader(reuters_id, story_folder)
-            storyline = dataloader.load_storyline()
-            shotlist = dataloader.load_shotlist()
-            story_title = dataloader.get_story_title()
-            video_file_path = dataloader.get_video_file_path()
+        if st.button("Generate Script"):
+            st.session_state["run"] = True
 
-            st.title(story_title)
-            st.video(str(video_file_path))
-            st.write(storyline.replace("$", "\$"))
-            st.write(shotlist.replace("$", "\$"))
-        
-            if st.button("Generate Script"):
-                st.session_state["run"] = True
+    if st.session_state["run"]:
+        print(reuters_id)
+        with st.status("Running"):
+            if not st.session_state["ran"]:
+                clips_folder = story_folder / "clips"
+                clip_manager = ClipManager(video_file_path, clips_folder, shotlist, Path("./assets/anchor-default.png"), anchor_voice_id, voiceover_voice_id, anchor_avatar_id, has_splash_screen=False, error_handler=error_handler)
+                script = NewsScript(storyline, shotlist, clip_manager, dataloader, folder=story_folder, error_handler=error_handler)
+                st.write("Splitting video")
+                error_handler.info("Splitting video")
+                clip_manager.split_video_into_clips()
+                st.write("Loading clips")
+                error_handler.info("Loading clips")
+                clip_manager.load_clips()
+                st.write("Transcribing clips")
+                error_handler.info("Transcribing clips")
+                clip_manager.transcribe_clips()
+                st.write("Matching clips to shotlist")
+                error_handler.info("Matching clips to shotlist")
+                clip_manager.match_clips()
+                st.write("Generating full descriptions")
+                error_handler.info("Generating full descriptions")
+                clip_manager.generate_full_descriptions(story_title)
 
-        if st.session_state["run"]:
-            print(reuters_id)
-            with st.status("Running"):
-                if not st.session_state["ran"]:
-                    clips_folder = story_folder / "clips"
-                    clip_manager = ClipManager(video_file_path, clips_folder, shotlist, Path("./assets/anchor-default.png"), anchor_voice_id, voiceover_voice_id, anchor_avatar_id, has_splash_screen=False, error_handler=error_handler)
-                    script = NewsScript(storyline, shotlist, clip_manager, dataloader, folder=story_folder, error_handler=error_handler)
-                    st.write("Splitting video")
-                    error_handler.info("Splitting video")
-                    clip_manager.split_video_into_clips()
-                    st.write("Loading clips")
-                    error_handler.info("Loading clips")
-                    clip_manager.load_clips()
-                    st.write("Transcribing clips")
-                    error_handler.info("Transcribing clips")
-                    clip_manager.transcribe_clips()
-                    st.write("Matching clips to shotlist")
-                    error_handler.info("Matching clips to shotlist")
-                    clip_manager.match_clips()
-                    st.write("Generating full descriptions")
-                    error_handler.info("Generating full descriptions")
-                    clip_manager.generate_full_descriptions(story_title)
+                st.write("Extracting facts")
+                error_handler.info("Extracting facts")
+                script.generate_facts()
+                st.write("Generating script")
+                error_handler.info("Generating script")
+                script.generate_script()
+                st.write("Generating lower thirds")
+                error_handler.info("Generating lower thirds")
+                script.generate_lower_thirds()
+                
+                st.write("Matching sots to clips")
+                error_handler.info("Matching sots to clips")
+                script.match_sot_clips()
 
-                    st.write("Extracting facts")
-                    error_handler.info("Extracting facts")
-                    script.generate_facts()
-                    st.write("Generating script")
-                    error_handler.info("Generating script")
-                    script.generate_script()
-                    st.write("Generating lower thirds")
-                    error_handler.info("Generating lower thirds")
-                    script.generate_lower_thirds()
-                    
-                    st.write("Matching sots to clips")
-                    error_handler.info("Matching sots to clips")
-                    script.match_sot_clips()
-
-                    st.session_state["clip_manager"] = clip_manager
-                    st.session_state["script"] = script
-                else:
-                    clip_manager = st.session_state["clip_manager"]
-                    script = st.session_state["script"]
-
-                    clip_manager.error_handler = error_handler
-                    script.error_handler = error_handler
-            
-            st.session_state["ran"] = True
-
-            with st.expander("Details"):
-                st.write(clip_manager.clips)
-
-            trt = script.get_total_read_time_seconds()
-            st.write(f"Estimated TRT: {trt}s")
-
-            st.subheader("Extracted Facts")
-            st.write(script.facts_list)
-
-            st.subheader("Final Story")
-
-            with st.container(height=800):
-                df = script.to_dataframe()
-                df = df[["type", "shot_id", "name", "text"]]
-                gb = GridOptionsBuilder.from_dataframe(df)
-                gb.configure_column("type", width=40, rowDrag=True, rowDragManaged=True, rowDragEntireRow = True, autoHeight=True, editable=True)
-                gb.configure_column("shot_id", width=30, autoHeight=True, editable=True)
-                gb.configure_column("name", width=50, wrapText=True, autoHeight=True, editable=True)
-                gb.configure_column("text", wrapText=True, autoHeight=True, editable=True)
-
-                gb.configure_grid_options(
-                    rowDragManaged=True,
-                    animateRows=True,
-                    scrollbar=True,
-                )
-                df = AgGrid(df, gridOptions=gb.build(), height=2000, update_mode=GridUpdateMode.MANUAL, fit_columns_on_grid_load=True, theme="alpine")["data"]
-                script.from_dataframe(df)
-
-            for section in script.get_sot_sections():
-                if not section.clip:
-                    st.warning(f"SOT Section {section.id} could not be matched to a clip. Skipping section.")
-                if section.match_type == "SPEECH":
-                    st.warning(f"SOT Section {section.id}'s transcript did not contain the given quote. Adding all detected speech.")
-                if section.match_type == "CLIP":
-                    st.warning(f"SOT Section {section.id}'s had no detected speech. Adding entire clip.")
-            
-            def reset_video():
-                st.session_state["video_run"] = False
-                st.session_state["video_ran"] = False
-            
-            high_res = st.toggle("High Resolution", value=False, on_change=reset_video)
-            output_resolution = (1920, 1080) if high_res else (640, 360)
-
-            if not st.session_state["video_run"]:
-                if st.button("Generate Video"):
-                    st.session_state["video_run"] = True
+                st.session_state["clip_manager"] = clip_manager
+                st.session_state["script"] = script
             else:
-                if st.button("Regenerate Video"):
-                    st.session_state["video_ran"] = False
+                clip_manager = st.session_state["clip_manager"]
+                script = st.session_state["script"]
+
+                clip_manager.error_handler = error_handler
+                script.error_handler = error_handler
+        
+        st.session_state["ran"] = True
+
+        with st.expander("Details"):
+            st.write(clip_manager.clips)
+
+        trt = script.get_total_read_time_seconds()
+        st.write(f"Estimated TRT: {trt}s")
+
+        st.subheader("Extracted Facts")
+        st.write(script.facts_list)
+
+        st.subheader("Final Story")
+
+        with st.container(height=800):
+            df = script.to_dataframe()
+            df = df[["type", "shot_id", "name", "text"]]
+            gb = GridOptionsBuilder.from_dataframe(df)
+            gb.configure_column("type", width=40, rowDrag=True, rowDragManaged=True, rowDragEntireRow = True, autoHeight=True, editable=True)
+            gb.configure_column("shot_id", width=30, autoHeight=True, editable=True)
+            gb.configure_column("name", width=50, wrapText=True, autoHeight=True, editable=True)
+            gb.configure_column("text", wrapText=True, autoHeight=True, editable=True)
+
+            gb.configure_grid_options(
+                rowDragManaged=True,
+                animateRows=True,
+                scrollbar=True,
+            )
+            df = AgGrid(df, gridOptions=gb.build(), height=2000, update_mode=GridUpdateMode.MANUAL, fit_columns_on_grid_load=True, theme="alpine")["data"]
+            script.from_dataframe(df)
+
+        for section in script.get_sot_sections():
+            if not section.clip:
+                st.warning(f"SOT Section {section.id} could not be matched to a clip. Skipping section.")
+            if section.match_type == "SPEECH":
+                st.warning(f"SOT Section {section.id}'s transcript did not contain the given quote. Adding all detected speech.")
+            if section.match_type == "CLIP":
+                st.warning(f"SOT Section {section.id}'s had no detected speech. Adding entire clip.")
+        
+        def reset_video():
+            st.session_state["video_run"] = False
+            st.session_state["video_ran"] = False
+        
+        high_res = st.toggle("High Resolution", value=False, on_change=reset_video)
+        output_resolution = (1920, 1080) if high_res else (640, 360)
+
+        if not st.session_state["video_run"]:
+            if st.button("Generate Video"):
+                st.session_state["video_run"] = True
+        else:
+            if st.button("Regenerate Video"):
+                st.session_state["video_ran"] = False
+        
+        if st.session_state["video_run"]:
+            with st.status("Running"):
+                if not st.session_state["video_ran"]:
+                    audio_processor = AudioProcessor(script, clip_manager, story_folder, error_handler)
+                    st.write("Generating anchor audio")
+                    if error_handler:
+                        error_handler.info("Generating anchor audio")
+                    audio_processor._process_anchor_audio()
+                    st.write("Generating SOT translations")
+                    if error_handler:
+                        error_handler.info("Generating SOT translations")
+                    audio_processor._generate_sot_translations()
+                    st.write("Adding broll placements")
+                    if error_handler:
+                        error_handler.info("Adding broll placements")
+                    audio_processor._add_broll_placements()
+
+                    st.write("Assembling video")
+                    error_handler.info("Assembling video")
+                    video_output_file = story_folder / "output.mp4"
+                    video_editor = VideoEditor(script, clip_manager, live_anchor, test_mode, music, Path("./assets/music-1.mp3"), output_resolution=output_resolution, logo_path=Path("./assets/lower_thirds_logo.png"), font=Path("./assets/Khand-SemiBold.ttf"), error_handler=error_handler)
+                    video_editor.assemble_video(output_file=video_output_file)
+                else:
+                    video_output_file = story_folder / "output.mp4"
             
-            if st.session_state["video_run"]:
-                with st.status("Running"):
-                    if not st.session_state["video_ran"]:
-                        audio_processor = AudioProcessor(script, clip_manager, story_folder, error_handler)
-                        st.write("Generating anchor audio")
-                        if error_handler:
-                            error_handler.info("Generating anchor audio")
-                        audio_processor._process_anchor_audio()
-                        st.write("Generating SOT translations")
-                        if error_handler:
-                            error_handler.info("Generating SOT translations")
-                        audio_processor._generate_sot_translations()
-                        st.write("Adding broll placements")
-                        if error_handler:
-                            error_handler.info("Adding broll placements")
-                        audio_processor._add_broll_placements()
+            st.session_state["video_ran"] = True
+            
+            with st.expander("Details"):
+                st.write(script)
 
-                        st.write("Assembling video")
-                        error_handler.info("Assembling video")
-                        video_output_file = story_folder / "output.mp4"
-                        video_editor = VideoEditor(script, clip_manager, live_anchor, test_mode, music, Path("./assets/music-1.mp3"), output_resolution=output_resolution, logo_path=Path("./assets/lower_thirds_logo.png"), font=Path("./assets/Khand-SemiBold.ttf"), error_handler=error_handler)
-                        video_editor.assemble_video(output_file=video_output_file)
-                    else:
-                        video_output_file = story_folder / "output.mp4"
-                
-                st.session_state["video_ran"] = True
-                
-                with st.expander("Details"):
-                    st.write(script)
-
-                st.video(str(video_output_file), autoplay=True)
+            st.video(str(video_output_file), autoplay=True)
 
 
 if __name__ == "__main__":
